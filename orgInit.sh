e1d468a1d273089702acec17ea89a02822635f41
Dave Scruggs <dscruggs@salesforce.com>
2021-08-13 16:36:50 -0400

    I can't include the usernames

--- orgInit.sh
+++ orgInit.sh
@@ -5,9 +5,9 @@
 # Create scratch org
 sfdx force:org:create -s -f config/project-scratch-def.json -a functions_recipes 
 # Deploy code
-sfdx force:source:push -f -u functions_recipes
+sfdx force:source:push -f 
 # Enable default user app access
-sfdx force:user:permset:assign -n FunctionsRecipes -u functions_recipes
+sfdx force:user:permset:assign -n FunctionsRecipes 
 # Auth to functions space (needs to be headless)
 #  sfdx login:functions:jwt --username testuser@mycompany.org --keyfile file.key --clientid 123456
 # sfdx login:functions:jwt --username=$HUBUSER --keyfile=certificates/server.key --clientid=$CONSUMER_KEY
@@ -16,4 +16,4 @@ sfdx env:create:compute -o functions_recipes -a fn_recipes
 # Deploy function(s)
 sfdx project:deploy:functions -o functions_recipes
 # Open the org
-sfdx force:org:open -p "/lightning/n/Functions" -u functions_recipes
+sfdx force:org:open -p "/lightning/n/Functions" 
