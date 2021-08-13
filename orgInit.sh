d41e53d7e9b0d4f499b80add6576cb8a82cbf473
Dave Scruggs <dscruggs@salesforce.com>
2021-08-13 16:34:23 -0400

    deployer being pedantic, being explicit w org name

--- orgInit.sh
+++ orgInit.sh
@@ -3,17 +3,17 @@
 # SFDX Hub Auth
 #  sfdx auth:jwt:grant --username=$HUBUSER --jwtkeyfile=certificates/server.key --clientid=$CONSUMER_KEY
 # Create scratch org
-sfdx force:org:create -s -f config/project-scratch-def.json -a functions_recipes
+sfdx force:org:create -s -f config/project-scratch-def.json -a functions_recipes 
 # Deploy code
-sfdx force:source:push -f
+sfdx force:source:push -f -u functions_recipes
 # Enable default user app access
-sfdx force:user:permset:assign -n FunctionsRecipes
+sfdx force:user:permset:assign -n FunctionsRecipes -u functions_recipes
 # Auth to functions space (needs to be headless)
 #  sfdx login:functions:jwt --username testuser@mycompany.org --keyfile file.key --clientid 123456
 # sfdx login:functions:jwt --username=$HUBUSER --keyfile=certificates/server.key --clientid=$CONSUMER_KEY
 # Create compute environment
-sfdx env:create:compute --connected-org functions_recipes --setalias fn_recipes
+sfdx env:create:compute -o functions_recipes -a fn_recipes
 # Deploy function(s)
-sfdx project:deploy:functions --connected-org functions_recipes
+sfdx project:deploy:functions -o functions_recipes
 # Open the org
-sfdx force:org:open -p "/lightning/n/Functions"
+sfdx force:org:open -p "/lightning/n/Functions" -u functions_recipes
