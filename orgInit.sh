e715cf8a18b245c067c9fba2f60d63cd3eed0709
Dave Scruggs <dscruggs@salesforce.com>
2021-09-17 09:56:32 -0400

    OrgInit fix + flexipage v53 fix

--- orgInit.sh
+++ orgInit.sh
@@ -1,19 +1,20 @@
 # Get env variables
 # source .env
-# SFDX Hub Auth
+# Optional headless SFDX Hub Auth
 #  sfdx auth:jwt:grant --username=$HUBUSER --jwtkeyfile=certificates/server.key --clientid=$CONSUMER_KEY
 # Create scratch org
 sfdx force:org:create -s -f config/project-scratch-def.json -a functions_recipes 
-# Deploy code
-sfdx force:source:push -f 
-# Enable default user app access
-sfdx force:user:permset:assign -n FunctionsRecipes 
-# Auth to functions space (needs to be headless)
-#  sfdx login:functions:jwt --username testuser@mycompany.org --keyfile file.key --clientid 123456
+# Deploy function before Apex; Apex can run typesafety check that function exists
+#
+# Auth to functions space (headless)
 # sfdx login:functions:jwt --username=$HUBUSER --keyfile=certificates/server.key --clientid=$CONSUMER_KEY
 # Create compute environment
 sfdx env:create:compute -o functions_recipes -a fn_recipes
 # Deploy function(s)
 sfdx project:deploy:functions -o functions_recipes
+# Deploy code
+sfdx force:source:push -f 
+# Enable default user app access
+sfdx force:user:permset:assign -n FunctionsRecipes 
 # Open the org
 sfdx force:org:open -p "/lightning/n/Functions" 
