aaa72f479568816eacde43d9013a741c7756573a
schandlergarcia <stephan.garcia@salesforce.com>
2021-10-07 15:51:31 -0700

    removed console

--- force-app/main/default/lwc/functionsRecipesNavigation/functionsRecipesNavigation.js
+++ force-app/main/default/lwc/functionsRecipesNavigation/functionsRecipesNavigation.js
@@ -3,10 +3,6 @@ import { LightningElement, api } from "lwc";
 export default class FunctionsRecipesNavigation extends LightningElement {
   @api definition;
   @api selectedItem;
-  connectedCallback() {
-    console.log(JSON.stringify(this.definition));
-    console.log(this.selectedItem);
-  }
 
   selectItem(event) {
     const name = event.currentTarget.name;
